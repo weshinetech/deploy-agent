ded13095f4a0cb4a14e319f766cb9f897968db1a
Ramkrishna Kulkarni <ramkrishna.kulkarni@gmail.com>
2022-02-28 17:32:21 +0530

    update install.sh to install anydesk and misc tools

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo apt-get -y update
 #
 # basic
 #
-sudo apt-get install -f unzip htop make curl
+sudo apt-get install -f unzip htop make curl net-tools
 
 
 
@@ -73,6 +73,7 @@ sudo systemctl disable systemd-resolved
 #
 sudo rm /etc/resolv.conf
 echo nameserver 8.8.8.8 | sudo tee /etc/resolv.conf
+sudo chattr +i /etc/resolv.conf
 
 
 
@@ -82,3 +83,13 @@ echo nameserver 8.8.8.8 | sudo tee /etc/resolv.conf
 sudo cp ./rc.local /etc/rc.local
 sudo cp ./configs/limits.conf /etc/security/limits.conf
 sudo cp ./configs/sysctl.conf /etc/sysctl.conf
+
+
+
+#
+# install anydesk
+#
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/$
+sudo apt-get -y update
+sudo apt-get install -y anydesk
