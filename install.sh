a94d2937d4b88586f776382e31d03bc485a7dbfb
Ramkrishna Kulkarni <ramkrishna.kulkarni@gmail.com>
2022-02-28 20:25:07 +0530

    install - anydesk correction

--- install.sh
+++ install.sh
@@ -90,6 +90,6 @@ sudo cp ./configs/sysctl.conf /etc/sysctl.conf
 # install anydesk
 #
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/$
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt-get -y update
 sudo apt-get install -y anydesk
